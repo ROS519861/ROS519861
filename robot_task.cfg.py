60451a1c1135115902f7246a5dccab7aaca4054f
ROS519861 <2281478@brunel.ac.uk>
2025-02-15 17:19:18 +0800

    Delete robot_task.cfg.py

--- robot_task.cfg.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-PACKAGE = "robot_task"
-from dynamic_reconfigure.parameter_generator_catkin import *
-
-def generate(gen):
-    gen.add("Kp", double_t, 0, "Proportional gain", 0.02, 0.0, 1.0)
-    gen.add("Ki", double_t, 0, "Integral gain", 0.001, 0.0, 0.1)
-    gen.add("Kd", double_t, 0, "Derivative gain", 0.005, 0.0, 0.1)
